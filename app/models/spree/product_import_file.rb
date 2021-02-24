3255a7a01770f72993e99843cbee35976c6871ea
Channa Ly <channa.info@gmail.com>
2021-02-24 21:36:11 +0700

    Close #2 product import creator

--- app/models/spree/product_import_file.rb
+++ app/models/spree/product_import_file.rb
@@ -8,12 +8,17 @@ class Spree::ProductImportFile < ApplicationRecord
   validates :file, presence: true
   validates :file, attached: true, content_type: %i[csv]
 
+  before_save :set_file_name
+
   validate :correct_mine_type
 
   ALLOW_FORMATS = %w(text/csv).freeze
 
-  def correct_mine_type
+  def set_file_name
+    self.file_name = file.blob.filename if file.attached?
+  end
 
+  def correct_mine_type
     if file.attached? && !file.content_type.in?(Spree::ProductImportFile::ALLOW_FORMATS)
       errors.add(:file, "format #{file.content_type} is not listed in #{Spree::ProductImportFile::ALLOW_FORMATS}")
     end
