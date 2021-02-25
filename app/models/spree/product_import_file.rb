fed9f8b219c96ebffc561ffd23be77554936bd61
Channa Ly <channa.info@gmail.com>
2021-02-25 11:37:38 +0700

    Close #5 validate product import file with shoulda-matchers and active_storage_validations

--- app/models/spree/product_import_file.rb
+++ app/models/spree/product_import_file.rb
@@ -1,27 +1,20 @@
-class Spree::ProductImportFile < ApplicationRecord
-  belongs_to :user, class_name: "#{Spree.user_class}"
-  has_one_attached :file
+module Spree
+  class ProductImportFile < Spree::Base
+    ALLOW_FORMATS = %w(text/csv application/vnd.openxmlformats-officedocument.spreadsheetml.sheet).freeze
+   
+    belongs_to :user, class_name: "#{Spree.user_class}"
+    has_one_attached :file
 
-  enum status: %i[pending :progress  success failed]
+    enum status: %i[pending in_progress success failed]
 
-  validates :name, presence: true
-  validates :file, presence: true
-  validates :file, attached: true, content_type: %i[csv]
+    validates :name, presence: true
+    validates :file, attached: true, content_type: ALLOW_FORMATS
 
-  before_save :set_file_name
+    before_save :set_file_name
 
-  validate :correct_mine_type
-
-  ALLOW_FORMATS = %w(text/csv).freeze
-
-  def set_file_name
-    self.file_name = file.blob.filename if file.attached?
-  end
-
-  def correct_mine_type
-    if file.attached? && !file.content_type.in?(Spree::ProductImportFile::ALLOW_FORMATS)
-      errors.add(:file, "format #{file.content_type} is not listed in #{Spree::ProductImportFile::ALLOW_FORMATS}")
+    def set_file_name
+      self.file_name = file.blob.filename if file.attached?
     end
+    
   end
-
 end
