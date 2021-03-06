ea544ac453044d6eba4d96010114b62abb135a50
Channa Ly <channa.info@gmail.com>
2021-03-06 11:44:45 +0700

    Close #21 integrate spree admin ui

--- app/jobs/product_import/processor_job.rb
+++ app/jobs/product_import/processor_job.rb
@@ -1,11 +1,13 @@
 module ProductImport
-  class ImporterJob < ::ApplicationJob
+  class ProcessorJob < ::ApplicationJob
     queue_as :default
 
     def perform(product_import_file_id)
       product_import_file = ::Spree::ProductImportFile.find(product_import_file_id)
-      return if product_import_file.nil?
-      ::ProductImport::Importer.new(product_import_file).call
+      
+      return if product_import_file.nil? || !product_import_file.active?
+
+      ::ProductImport::Processor.new(product_import_file).call
     end
   end
 end
