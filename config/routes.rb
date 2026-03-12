79114898995677f3723e55ee2044a26f0a82865c
Namoral Thy <thynamoral18@gmail.com>
2026-03-12 16:10:08 +0700

    feat: refactor routes and asset-precompile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
   namespace :admin do
-    resources :product_import_files
+    resources :product_import_files do
+      collection do
+        patch :bulk_update
+      end
+    end
   end
 
 end
